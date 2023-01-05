fd49b347566445e780add0b4e79eaed5132d487d
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2023-01-05 09:49:47 -0600

    PartsMap and TextsMap now delegate to Map.

--- lib/src/main/groovy/com/jessebrault/ssg/part/PartsMap.groovy
+++ lib/src/main/groovy/com/jessebrault/ssg/part/PartsMap.groovy
@@ -1,22 +1,18 @@
 package com.jessebrault.ssg.part
 
+import groovy.transform.ToString
+
+@ToString(includeFields = true)
 class PartsMap {
 
+    @Delegate
     private final Map<String, EmbeddablePart> partsMap = [:]
 
     PartsMap(Collection<Part> parts) {
         Objects.requireNonNull(parts)
         parts.each {
-            this.partsMap.put(it.name, new EmbeddablePart(it))
+            this.put(it.name, new EmbeddablePart(it))
         }
-    }
-
-    EmbeddablePart get(String name) {
-        this.partsMap.get(Objects.requireNonNull(name))
-    }
-
-    EmbeddablePart getAt(String name) {
-        this.get(Objects.requireNonNull(name))
     }
 
 }
--- lib/src/main/groovy/com/jessebrault/ssg/text/TextsMap.groovy
+++ lib/src/main/groovy/com/jessebrault/ssg/text/TextsMap.groovy
@@ -1,22 +1,18 @@
 package com.jessebrault.ssg.text
 
+import groovy.transform.ToString
+
+@ToString(includeFields = true)
 class TextsMap {
 
+    @Delegate
     private final Map<String, EmbeddableText> textsMap = [:]
 
     TextsMap(Collection<Text> texts) {
         Objects.requireNonNull(texts)
         texts.each {
-            this.textsMap.put(it.path, new EmbeddableText(it))
+            this.put(it.path, new EmbeddableText(it))
         }
-    }
-
-    EmbeddableText get(String path) {
-        this.textsMap.get(Objects.requireNonNull(path))
-    }
-
-    EmbeddableText getAt(String path) {
-        this.get(Objects.requireNonNull(path))
     }
 
 }
