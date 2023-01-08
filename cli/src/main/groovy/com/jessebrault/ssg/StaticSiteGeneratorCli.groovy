09c174415bd21e6268c0940cde050ce57b58d40b
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2023-01-08 10:05:13 -0600

    Experimenting with programmatic log levels.

--- cli/src/main/groovy/com/jessebrault/ssg/StaticSiteGeneratorCli.groovy
+++ cli/src/main/groovy/com/jessebrault/ssg/StaticSiteGeneratorCli.groovy
@@ -14,14 +14,56 @@ import com.jessebrault.ssg.text.MarkdownFrontMatterGetter
 import com.jessebrault.ssg.text.MarkdownTextRenderer
 import com.jessebrault.ssg.text.TextFileTextsProvider
 import com.jessebrault.ssg.text.TextType
-import org.slf4j.Logger
-import org.slf4j.LoggerFactory
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import org.apache.logging.log4j.core.config.Configuration
+import org.apache.logging.log4j.spi.LoggerContext
+import picocli.CommandLine
 
-class StaticSiteGeneratorCli {
+import java.util.concurrent.Callable
 
-    private static final Logger logger = LoggerFactory.getLogger(StaticSiteGeneratorCli)
+@CommandLine.Command(
+        name = 'ssg',
+        mixinStandardHelpOptions = true,
+        version = '0.0.1-SNAPSHOT',
+        description = 'Generates a set of html files from a given configuration.'
+)
+class StaticSiteGeneratorCli implements Callable<Integer> {
+
+    private static final Logger logger = LogManager.getLogger(StaticSiteGeneratorCli)
+
+    static class LogLevel {
+        @CommandLine.Option(names = ['--info']) boolean info
+        @CommandLine.Option(names = ['--debug']) boolean debug
+        @CommandLine.Option(names = ['--trace']) boolean trace
+    }
 
     static void main(String[] args) {
+        System.exit(new CommandLine(StaticSiteGeneratorCli).execute(args))
+    }
+
+    @CommandLine.ArgGroup(exclusive = true)
+    private LogLevel logLevel
+
+    @Override
+    Integer call() throws Exception {
+        def context = (org.apache.logging.log4j.core.LoggerContext) LogManager.getContext(false)
+        def configuration = context.getConfiguration()
+        def rootLoggerConfig = configuration.getRootLogger()
+
+        if (this.logLevel.info) {
+            rootLoggerConfig.level = Level.INFO
+        } else if (this.logLevel.debug) {
+            rootLoggerConfig.level = Level.DEBUG
+        } else if (this.logLevel.trace) {
+            rootLoggerConfig.level = Level.TRACE
+        } else {
+            rootLoggerConfig.level = Level.WARN
+        }
+
+        context.updateLoggers()
+
         def markdownText = new TextType(['.md'], new MarkdownTextRenderer(), new MarkdownFrontMatterGetter())
         def gspTemplate = new TemplateType(['.gsp'], new GspTemplateRenderer())
         def gspPart = new PartType(['.gsp'], new GspPartRenderer())
@@ -51,6 +93,7 @@ class StaticSiteGeneratorCli {
 
         def ssg = new SimpleStaticSiteGenerator(config)
         ssg.generate(new File('build'), globals)
+        return 0
     }
 
 }
