3a1ecfe5244aa1b09d094bcb74c29e3c8acd06cb
JesseBrault0709 <62299747+JesseBrault0709@users.noreply.github.com>
2023-06-16 15:40:01 +0200

    Added a TextProvider with filter.

--- api/src/main/groovy/com/jessebrault/ssg/text/TextsProviders.groovy
+++ api/src/main/groovy/com/jessebrault/ssg/text/TextsProviders.groovy
@@ -6,23 +6,37 @@ import com.jessebrault.ssg.util.ExtensionUtil
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
+import java.util.function.BiPredicate
+
 final class TextsProviders {
 
     private static final Logger logger = LoggerFactory.getLogger(TextsProviders)
 
     static CollectionProvider<Text> from(File textsDir, Collection<TextType> types) {
+        from(textsDir, types) { file, relativePath -> true }
+    }
+
+    static CollectionProvider<Text> from(
+            File textsDir,
+            Collection<TextType> textTypes,
+            BiPredicate<File, String> filter
+    ) {
         CollectionProviders.fromDirectory(textsDir) { file, relativePath ->
-            def extension = ExtensionUtil.getExtension(relativePath)
-            if (extension) {
-                def textType = types.find { it.ids.contains(extension) }
-                if (!textType) {
-                    logger.warn('there is no TextType for file {}; skipping', file)
-                    return null
+            if (filter.test(file, relativePath)) {
+                def extension = ExtensionUtil.getExtension(relativePath)
+                if (extension) {
+                    def textType = textTypes.find { it.ids.contains(extension) }
+                    if (!textType) {
+                        logger.warn('there is no TextType for file {}; skipping', file)
+                        return null
+                    } else {
+                        return new Text(relativePath, textType, file.getText())
+                    }
                 } else {
-                    return new Text(relativePath, textType, file.getText())
+                    logger.debug('there is no extension for file {}; skipping', file)
+                    return null
                 }
             } else {
-                logger.debug('there is no extension for file {}; skipping', file)
                 return null
             }
         }
